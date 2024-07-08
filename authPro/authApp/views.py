3049659716f11a441411b1ab4fac014197a64b3a
anozie5 <chinemeremanozie5@gmail.com>
2024-07-08 15:27:49 +0100

    done

--- authPro/authApp/views.py
+++ authPro/authApp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+# home view
+def home_view(request):
+    return render (request, 'authApp/home.html')
+
