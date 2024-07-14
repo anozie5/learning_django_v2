cddc0af440675cfe75956d211243693db7ca6f21
anozie5 <chinemeremanozie5@gmail.com>
2024-07-14 19:02:26 +0100

    Still working

--- authPro/authApp/urls.py
+++ authPro/authApp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home_view, name = 'home'),
     path('login/', views.log_in_view, name = 'login'),
     path('signup/', views.sign_up_view, name = 'signup'),
-    path('create/<int:id>/', views.create_post, name = 'create'),
+    path('create/<int:ui>/', views.create_post, name = 'create'),
     path('delete/<int:ui>/', views.delete_post, name = 'delete')
 ]
